a0a43e8c5d63837b0979042b32864580f56b4a44
prapul <prapulpoonacha@gmail.com>
2016-12-20 11:12:19 +0530

    Program to swap values of two variables
    Swaps values

---
+++ Assignment_3.java
@@ -0,0 +1,17 @@
+package prapul;
+
+public class Assignment_3 {
+	public static void main(String[] args) {
+int a,b;
+a=10;
+b=20;
+System.out.println("The initial value of a is "+a);
+System.out.println("The initial value of b is "+b);
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println("The swapped value of a is "+a);
+System.out.println("The swapped value of b is "+b);
+
+	}
+}
